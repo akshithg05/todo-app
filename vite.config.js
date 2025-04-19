a5ccdafdda7f726b02df008ac4b201e363f38ee4
Akshith G <akshithg01@gmail.com>
2025-04-20 02:04:03 +0530

    update vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: "dist", // Output directory for the production build (default is 'dist')
+    assetsDir: "assets", // Directory to store static assets like images, etc.
+    sourcemap: true, // Optional: For debugging if needed
+  },
+});
